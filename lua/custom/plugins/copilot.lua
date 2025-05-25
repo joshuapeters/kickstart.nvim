8c53ee3a3ad060e6eb506eb6e91ed5b2c2d92ad7
Josh Peters <jgpeters717@gmail.com>
2025-05-25 10:44:09 -0400

    remove code companion

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -11,25 +11,4 @@ return {
       }
     end,
   },
-  {
-    'olimorris/codecompanion.nvim',
-    opts = {},
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    config = function()
-      require('codecompanion').setup {
-        adapters = {
-          openai = function()
-            return require('codecompanion.adapters').extend('openai', {
-              env = {
-                api_key = os.getenv 'ANTHROPIC_API_KEY',
-              },
-            })
-          end,
-        },
-      }
-    end,
-  },
 }
