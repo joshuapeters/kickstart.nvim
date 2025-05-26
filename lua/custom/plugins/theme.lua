fc670a4341c7f047e5099faf6f12814d9986c068
Josh Peters <jgpeters717@gmail.com>
2025-05-26 10:14:41 -0400

    transparent catpuccin background

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -4,7 +4,11 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      require('catppuccin').setup()
+      require('catppuccin').setup {
+        transparent_background = true,
+        no_italic = true,
+        no_bold = true,
+      }
       vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
